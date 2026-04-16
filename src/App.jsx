129fcc8ba4d8ce5ad4bc68e8120e04ab7841c101
gabdani11 <gabrielang56799@gmail.com>
2026-04-16 14:37:29 +0530

    initial version

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.scss'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <img src="psyduck.webp" alt="psyduckImg" />
+     <h1>This page is under construction, Check this site later.</h1>
+    </div>
+  )
+}
+
+export default App
